2741f3b63054dacab212e8e4a3171aefad8b835d
tallguy188 <cksghd4220@naver.com>
2023-05-18 00:19:48 +0900

    [feat]CORS설정

--- src/main/java/com/mbti/common/config/AppConfig.java
+++ src/main/java/com/mbti/common/config/AppConfig.java
@@ -5,6 +5,7 @@ import com.mbti.common.interceptor.TokenInterceptor;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,8 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @AllArgsConstructor
 public class AppConfig implements WebMvcConfigurer {
     private final TokenInterceptor tokenInterceptor;
-
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 인터셉터를 등록하고 경로 패턴을 설정
@@ -23,9 +22,14 @@ public class AppConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/login")
                 .excludePathPatterns("/register")
                 .excludePathPatterns("/swagger-ui/**"); // Swagger UI 경로 제외
-
-
-
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("*")  //Authorization
+                .allowCredentials(true);
+    }
 }
