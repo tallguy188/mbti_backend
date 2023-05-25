d8335ca5a951c6620f1971af6b2df5d56b187ca3
tallguy188 <cksghd4220@naver.com>
2023-05-25 17:05:17 +0900

    [feat]securityconfig수정

--- src/main/java/com/mbti/common/config/SecurityConfig.java
+++ src/main/java/com/mbti/common/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 @Configuration
 @Log4j2
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig{
 
 
 //    @Override
@@ -41,30 +41,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//        return httpSecurity       // SecurityFilterChain에서 요청에 접근할 수 있어서 인증, 인가 서비스에 사용
-//                .httpBasic().disable()  // http basic auth 기반으로 로그인 인증창이 뜬다. 기본 인증을 이용하지 않으려면 .disable()을 추가해준다
-//                .csrf().disable()  // csrf, api server이용시 .disable (html tag를 통한 공격)
-//                .cors()   // 다른 도메인의 리소스에 대해 접근이 허용되는지 체크
-//                .and()  // 묶음 구분(httpBasic(),crsf,cors가 한묶음)
-//                .authorizeRequests()  //각 경로 path별 권한 처리
-//                .antMatchers("/register/**").permitAll()
-//                .antMatchers("/login/**").permitAll()
-//                .antMatchers("/min").permitAll()
-//                .antMatchers("/createChatRoom").permitAll()
-//                .antMatchers("/receive").permitAll()
-//                .antMatchers("/send").permitAll()
-//                .antMatchers("/chat").permitAll()
-//                .antMatchers("/").permitAll()
-//                // .antMatchers("/**").permitAll()
-//                .and()
-//                .sessionManagement()  //세션 관리 기능
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //jwt사용하는 경우 씀(STATELESS는 인증 정보를 서버에 담지 않는다.)
-//                //.addFilterBefore(new JwtTokenFilter(userService,secretKey), UsernamePasswordAuthenticationFilter.class)
-//                .and()
-//                .build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity       // SecurityFilterChain에서 요청에 접근할 수 있어서 인증, 인가 서비스에 사용
+                .httpBasic().disable()  // http basic auth 기반으로 로그인 인증창이 뜬다. 기본 인증을 이용하지 않으려면 .disable()을 추가해준다
+                .csrf().disable()  // csrf, api server이용시 .disable (html tag를 통한 공격)
+                .cors()   // 다른 도메인의 리소스에 대해 접근이 허용되는지 체크
+                .and()  // 묶음 구분(httpBasic(),crsf,cors가 한묶음)
+                .authorizeRequests()  //각 경로 path별 권한 처리
+                .antMatchers("/register/**").permitAll()
+                .antMatchers("/login/**").permitAll()
+                .antMatchers("/min").permitAll()
+                .antMatchers("/chat").permitAll()
+                .antMatchers("/").permitAll()
+                // .antMatchers("/**").permitAll()
+                .and()
+                .sessionManagement()  //세션 관리 기능
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //jwt사용하는 경우 씀(STATELESS는 인증 정보를 서버에 담지 않는다.)
+                //.addFilterBefore(new JwtTokenFilter(userService,secretKey), UsernamePasswordAuthenticationFilter.class)
+                .and()
+                .build();
+    }
 
 //    @Override
 //     protected void configure(HttpSecurity http) throws  Exception {
@@ -75,27 +72,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic().disable()
-                .csrf().disable()
-                .cors()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/register/**").permitAll()
-                .antMatchers("/login/**").permitAll()
-                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .invalidateHttpSession(true);
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                .cors()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/register/**").permitAll()
+//                .antMatchers("/login/**").permitAll()
+//                .antMatchers("/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/")
+//                .invalidateHttpSession(true);
+//    }
 
 
 }
